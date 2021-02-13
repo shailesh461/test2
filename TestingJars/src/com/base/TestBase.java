67a572d50e3649dd23881856cd5ff6f4cac34ba4
DELL <DELL@DESKTOP-3627H0G>
2021-02-13 23:11:51 +0530

    first push

---
+++ TestingJars/src/com/base/TestBase.java
@@ -0,0 +1,15 @@
+package com.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WebDriver driver=new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "D:\\chromedriver");
+		System.out.println("print");
+
+	}
+
+}
